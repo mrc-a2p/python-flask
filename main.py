da0f99cf95670cdabd1c1649abf3975e7735a875
Marc Araujo <mrc.a2p@gmail.com>
2019-08-14 18:55:05 +0200

    template

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ from flask import Flask, request, make_response, redirect, render_template
 #Crear nueva instancia de flask
 app = Flask(__name__)
 
+############################
+# arrays 
+#############################
+
+perros = [
+    "labrador", 
+    "pitbull", 
+    "chiwuwa"
+]
+
+
 
 ############################
 # Creación de rutas 
@@ -25,9 +36,29 @@ def index():
 def hello():
     user_ip = request.cookies.get("user_ip")
     #return "Hola mundo,{}" .format(user_ip)
-    #renderizamos el codigo en una vista previamente creado en una carpeta de templates 
-    return render_template("hello.html", user_ip=user_ip)
+    #Creamos un diccionario con las variables que vamos a utilizar
+    context = { 
+        "user_ip" : user_ip,
+        "perros" : perros
+    }
 
+    #renderizamos el codigo en una vista previamente creado en una carpeta de templates 
+    #Los asteriscos se deben a que locals() nos regresa un dict con las variables del contexto, pero render_template solo resive un argumento, asi que pasamos el diccionario con key yvaluesen forma de argumentos opcionales. 
+    #Esto es util cuando empezamos a tener muchos datos enel entorno.
+    #Expande el diccionario e hace un recorrido dato por dato 
+    return render_template("hello.html", **context)
+
+
+
+
+
+###################  mas
 @app.route("/marc")
 def marc():
     return render_template("marc.html")  
+
+
+@app.route("/info")
+def info():
+    #return "lo que sea"
+    return render_template("info.html")
